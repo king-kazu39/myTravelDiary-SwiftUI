0e2af4267b99be060712cf31b3962a4db16ac89c
kazuya <kazuya@kazuyanoMacBook-Pro.local>
2020-09-01 22:40:26 +0900

    新規プロジェクト作成

---
+++ myTravelDiary-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myTravelDiary-SwiftUI
+//
+//  Created by kazuya on 2020/09/01.
+//  Copyright © 2020 kazuya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
